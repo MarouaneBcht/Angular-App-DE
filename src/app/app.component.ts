f2b1fccc0f8c0db1b7ba32b85ba9a63cd8396082
BOUCHTAOUI_MAROUANE <mrnb778@gmail.com>
2023-05-30 10:02:25 +0100

    V1

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  actions : Array<any> = [
+    {title : "Home" , route : "/home" },
+    {title : "Products" , route : "/products" },
+    {title : "New Product" , route : "/newProduct" }
+
+    ];
+
+  currentAction : any;
+
+  setCurrentAction(action : any) {
+    this.currentAction = action;
+  }
+}
